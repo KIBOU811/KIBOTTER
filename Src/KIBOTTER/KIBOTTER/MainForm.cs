88215b4102089d6d40605cb2b1dfa8f1c01fda4c
きーぼう <kibou811@gmail.com>
2018-06-15 00:09:31 +0900

    [fix]予約投稿ができるようnullエラーなどを修正

--- Src/KIBOTTER/KIBOTTER/MainForm.cs
+++ Src/KIBOTTER/KIBOTTER/MainForm.cs
@@ -29,7 +29,7 @@ namespace KIBOTTER
 
         private bool _isClosing;
 
-        public List<ScheduledTweetClass> ScheduledTweetList = new List<ScheduledTweetClass>();
+        public List<ScheduledTweetClass> ScheduledTweetList;
 
         private KIBOTTERSettingClass _kibotterSetting = new KIBOTTERSettingClass();
 
@@ -79,6 +79,9 @@ namespace KIBOTTER
                     sw.Write("");
                 }
             }
+
+            if (ScheduledTweetList == null)
+                ScheduledTweetList = new List<ScheduledTweetClass>();
 
             filePath = $"{folder}\\Settings.json";
             if (File.Exists(filePath))
